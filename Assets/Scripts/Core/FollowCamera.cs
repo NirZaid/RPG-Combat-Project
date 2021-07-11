b290389fa77f42297c862830fe2b2fa5b2d97472
Nir9000 <nir9000@gmail.com>
2021-07-11 16:41:54 +0300

    Implemented Player Movement

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.transform.position;
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private Animator _animator;
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+
+    public void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
+    public void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        _animator.SetFloat("forwardSpeed", speed);
+
+    }
+}
