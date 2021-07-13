da72925930d8cf0f0eceb814e70a232f712cb5a4
Nir9000 <nir9000@gmail.com>
2021-07-13 09:58:03 +0300

    Implemented Basic AI

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using RPG.Combat;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] private float chaseDistance = 5f;
+        private Fighter _fighter;
+
+        private void Start()
+        {
+            _fighter = GetComponent<Fighter>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            if (InAttackRange() && _fighter.CanAttack(player))
+            {
+                _fighter.Attack(player);
+            }
+            else
+            {
+                _fighter.Cancel();
+            }
+            
+        }
+        
+        private bool InAttackRange()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(transform.position, player.transform.position) < chaseDistance;
+        }
+    }
+        
+        
+}
+
+       
+
+
